381c297552852e8d6ff05b37951fa074720aa349
Tom Prouse <tom.prouse@gmail.com>
2017-06-18 15:08:36 +1000

    use mutagen to get values for proper bitrate calcs

--- main.py
+++ main.py
@@ -10,6 +10,8 @@
 #       to keep metadata clean, it is minimal and consists only of book name, author name
 #   it moves the m4b file into the output folder and moves the mp3 audiobook into the completed folder
 
+from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
 import os
 import shlex
 import subprocess
@@ -26,10 +28,10 @@ for entry in os.listdir("input"):
 
     # the author name is the part of the folder name before the first dash
     # the book name is everything after the first dash
-    bookdata = entry.partition("-")
-    authorname = bookdata[0]
-    bookname = bookdata[2]
-    print("Author=", authorname, " Title=", bookname)
+    bookData = entry.partition("-")
+    authorName = bookData[0]
+    bookName = bookData[2]
+    print("Author=", authorName, " Title=", bookName)
 
     # create working folder
     os.makedirs("working/" + entry)
@@ -39,45 +41,57 @@ for entry in os.listdir("input"):
     rawfilelist = os.listdir("input/" + entry)
     rawfilelist.sort()
 
-    filecounter = 0
+    fileCounter = 0
     referenceToPopens = {}
+
+    bitrate = ''
 
     for rawfile in rawfilelist:
         if rawfile.endswith('.mp3'):
-            filecounter += 1
+            fileCounter += 1
 
-            # TODO fix to use appropriate bitrate
-            ##################################################################
-            #            # <22.05 KHz # 22.05KHz   # 44.1 KHz   # >44.1KHz   #
-            ##################################################################
-            # Stereo     #  48 Kbps   #  64 Kbps   #  96 Kbps   # 112 Kbps   #
-            # Mono       #  48 Kbps   #  48 Kbps   #  64 Kbps   #  80 Kbps   #
-            ##################################################################
-            # TODO determine input channels - stereo or mono?
-            input_channels = 1  # mono
+            # read details from first file - assumption all files encoded similarly
+            if bitrate == '':
+                # Table of appropriate bit rates for spoken word content
+                ##################################################################
+                #            # <22.05 KHz # 22.05KHz   # 44.1 KHz   # >44.1KHz   #
+                ##################################################################
+                # Stereo     #  48 Kbps   #  64 Kbps   #  96 Kbps   # 112 Kbps   #
+                # Mono       #  32 Kbps   #  48 Kbps   #  64 Kbps   #  80 Kbps   #
+                ##################################################################
 
-            # TODO determine input frequency in Hz
-            input_freq = 44100
+                mp3Info = MP3('input/' + entry + '/' + rawfile)
 
-            bitrate = "64k"  # default - always overridden
-            if input_channels > 1 and input_freq > 44100:
-                bitrate = "112k"
-            elif input_freq > 44100:
-                bitrate = "80k"
-            elif input_channels > 1 and input_freq > 44000:
-                bitrate = "96k"
-            elif input_freq > 44000:
-                bitrate = "64k"
-            elif input_channels > 1 and input_freq > 22000:
-                bitrate = "64k"
-            elif input_freq > 22000:
-                bitrate = "48k"
-            elif input_channels > 1:
-                bitrate = "48k"
-            else:
-                bitrate = "48k"
+                # TODO determine input channels - stereo or mono?
+                input_channels = mp3Info.info.channels
 
-            outputFilename = workingFolder + "outputfile%03d.aac" % filecounter
+                # TODO determine input frequency in Hz
+                input_freq = mp3Info.info.sample_rate
+
+                bitrate = "64k"  # default - should always be overridden
+                if input_channels > 1 and input_freq > 44100:
+                    bitrate = "112k"
+                elif input_freq > 44100:
+                    bitrate = "80k"
+                elif input_channels > 1 and input_freq > 44000:
+                    bitrate = "96k"
+                elif input_freq > 44000:
+                    bitrate = "64k"
+                elif input_channels > 1 and input_freq > 22000:
+                    bitrate = "64k"
+                elif input_freq > 22000:
+                    bitrate = "48k"
+                elif input_channels > 1:
+                    bitrate = "48k"
+                else:
+                    bitrate = "32k"
+
+                bitrateMessage = "As the input file is " + str(input_freq) + "Hz"
+                bitrateMessage += " and has " + str(input_channels) + " channel"
+                bitrateMessage += ", use a bitrate of " + bitrate
+                print(bitrateMessage)
+
+            outputFilename = workingFolder + "outputfile%03d.aac" % fileCounter
 
             commandline = 'ffmpeg -i "input/' + entry + '/' + rawfile + '" -loglevel panic -y -c:a aac -b:a  '
             commandline += bitrate + ' "' + outputFilename + '"'
@@ -86,7 +100,7 @@ for entry in os.listdir("input"):
 
             # launch the recode in the background..
             p = subprocess.Popen(args)
-            referenceToPopens[filecounter] = p
+            referenceToPopens[fileCounter] = p
 
     # wait here until all encodes are completed
     filesComplete = 0
@@ -94,7 +108,7 @@ for entry in os.listdir("input"):
         # prevent thrashing
         time.sleep(.5)
         for key, value in referenceToPopens.items():
-            outputLine = "\rEncoding to aac. " + str(filesComplete) + "/" + str(filecounter) + " completed"
+            outputLine = "\rEncoding to aac " + str(filesComplete) + "/" + str(fileCounter)
             sys.stdout.write(outputLine)
 
             if value.poll() is None:
@@ -105,13 +119,13 @@ for entry in os.listdir("input"):
 
                 # update counter
                 filesComplete += 1
-                outputLine = "\rEncoding to aac. " + str(filesComplete) + "/" + str(filecounter) + " completed"
+                outputLine = "\rEncoding to aac " + str(filesComplete) + "/" + str(fileCounter)
                 sys.stdout.write(outputLine)
                 break
 
         # when all are complete, move to the next audiobook
-        if filesComplete >= filecounter:
-            outputLine = "\rEncoding to aac. " + str(filesComplete) + "/" + str(filecounter) + " completed"
+        if filesComplete >= fileCounter:
+            outputLine = "\rEncoding to aac " + str(filesComplete) + "/" + str(fileCounter)
             sys.stdout.write(outputLine)
             sys.stdout.flush()
             break
@@ -122,10 +136,10 @@ for entry in os.listdir("input"):
     mergeCommandlineTail = '" -c copy ' + '"' + workingFolder + entry + '.m4a"'
 
     fileMergeIterator = 0
-    while fileMergeIterator < filecounter:
+    while fileMergeIterator < fileCounter:
         fileMergeIterator += 1
         mergeCommandlineBody += workingFolder + "outputfile%03d.aac" % fileMergeIterator
-        if fileMergeIterator < filecounter:
+        if fileMergeIterator < fileCounter:
             mergeCommandlineBody += "|"
 
     mergeCommand = mergeCommandlineHead + mergeCommandlineBody + mergeCommandlineTail
@@ -134,16 +148,24 @@ for entry in os.listdir("input"):
     args = shlex.split(mergeCommand)
 
     # Now run the merge process for this book - results in m4a containing all aac file content
-    p = subprocess.Popen(args)
-
-
-    # set metadata
-    # - set author, book name
-    # - TODO set cover image
+    subprocess.run(args)
 
     # rename to m4b
+    os.rename(workingFolder + entry + '.m4a', workingFolder + entry + '.m4b')
 
-    # move source to completed folder
+    # set metadata
+    mp4Info = MP4(workingFolder + entry + '.m4b')
+
+    # set minimal tags - DOESNT SEEM TO WORK?
+    mp4Info['\\xa9ART'] = authorName
+    mp4Info['\\xa9alb'] = bookName
+
+    # - TODO set cover image
+
+    mp4Info.save()
+
+
+    # move source folder to the completed folder
 
     # move m4b to output folder
 

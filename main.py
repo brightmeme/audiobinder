c80ea8805339f95ef62ce387a0b08c1d13318817
Tom Prouse <tom.prouse@esamconsulting.com.au>
2017-07-26 17:05:59 +1000

    refactor in preparation for changing parallelism so multiple books are encoded at the same time

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ for entry in os.listdir("input"):
     # the author name is the part of the folder name before the first dash
     # the book name is everything after the first dash
     bookData = entry.partition("-")
-    authorName = bookData[0].strip()
-    bookName = bookData[2].strip()
-    print("Author=", authorName, " Title=", bookName)
+    author_name = bookData[0].strip()
+    book_name = bookData[2].strip()
+    print("Author=", author_name, " Title=", book_name)
 
     # create working folder
     os.makedirs("working/" + entry)
@@ -55,44 +55,7 @@ for entry in os.listdir("input"):
 
             # read details from first file - assumption all files encoded similarly
             if bitrate == '':
-                # Table of appropriate bit rates for spoken word content
-                ##################################################################
-                #            # <22.05 KHz # 22.05KHz   # 44.1 KHz   # >44.1KHz   #
-                ##################################################################
-                # Stereo     #  32 Kbps   #  48 Kbps   #  64 Kbps   #  96 Kbps   #
-                # Mono       #  32 Kbps   #  32 Kbps   #  48 Kbps   #  80 Kbps   #
-                ##################################################################
-
-                mp3Info = MP3('input/' + entry + '/' + rawfile)
-
-                # TODO determine input channels - stereo or mono?
-                input_channels = mp3Info.info.channels
-
-                # TODO determine input frequency in Hz
-                input_freq = mp3Info.info.sample_rate
-
-                bitrate = "64k"  # default - should always be overridden
-                if input_channels > 1 and input_freq > 44100:
-                    bitrate = "96k"
-                elif input_freq > 44100:
-                    bitrate = "80k"
-                elif input_channels > 1 and input_freq > 44000:
-                    bitrate = "64k"
-                elif input_freq > 44000:
-                    bitrate = "48k"
-                elif input_channels > 1 and input_freq > 22000:
-                    bitrate = "32k"
-                elif input_freq > 22000:
-                    bitrate = "32k"
-                elif input_channels > 1:
-                    bitrate = "32k"
-                else:
-                    bitrate = "32k"
-
-                bitrateMessage = "As the input file is " + str(input_freq) + "Hz"
-                bitrateMessage += " and is " + str(input_channels) + " channel"
-                bitrateMessage += ", use a bitrate of " + bitrate
-                print(bitrateMessage)
+                bitrate = determine_bitrate_from_mp3_file('input/' + entry + '/' + rawfile)
 
             outputFilename = workingFolder + "outputfile%03d.aac" % fileCounter
 
@@ -156,35 +119,8 @@ for entry in os.listdir("input"):
     # rename to m4b
     os.rename(workingFolder + entry + '.m4a', workingFolder + entry + '.m4b')
 
-    # set metadata
-    mp4Info = MP4(workingFolder + entry + '.m4b')
-
-    # set minimal tags
-    mp4Info['\xa9ART'] = authorName
-    mp4Info['\xa9alb'] = bookName
-
-    # set cover image
-    coverType = "none"
-    coverFile = ""
-    if os.path.isfile("input/" + entry + "/cover.jpg"):
-        coverFile = "input/" + entry + "/cover.jpg"
-        coverType = 'jpg'
-    if os.path.isfile("input/" + entry + "/cover.png"):
-        coverFile = "input/" + entry + "/cover.png"
-        coverType = 'png'
-
-    if coverType != "none":
-        with open(coverFile, "rb") as f:
-            if coverType == "jpg":
-                mp4Info['covr'] = [
-                    MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_JPEG)
-                ]
-            else:
-                mp4Info['covr'] = [
-                    MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_PNG)
-                ]
-
-    mp4Info.save()
+    # TODO test parameters
+    set_metadata_on_m4b_audiobook_file(workingFolder + entry + '.m4b', author_name, book_name, workingFolder + entry)
 
     # move source folder to the archive folder
     shutil.move("input/" + entry, "archive/" + entry)
@@ -212,3 +148,75 @@ for entry in os.listdir("input"):
 
     # clear working folder
     shutil.rmtree(workingFolder)
+
+
+def determine_bitrate_from_mp3_file(self, mp3_file_path):
+    # Table of appropriate bit rates for spoken word content
+    ##################################################################
+    #            # <22.05 KHz # 22.05KHz   # 44.1 KHz   # >44.1KHz   #
+    ##################################################################
+    # Stereo     #  32 Kbps   #  48 Kbps   #  64 Kbps   #  96 Kbps   #
+    # Mono       #  32 Kbps   #  32 Kbps   #  48 Kbps   #  80 Kbps   #
+    ##################################################################
+
+    mp3Info = MP3(mp3_file_path)
+
+    # determine input channels - stereo or mono?
+    input_channels = mp3Info.info.channels
+
+    # determine input frequency in Hz
+    input_freq = mp3Info.info.sample_rate
+
+    bitrate = "64k"  # default - should always be overridden
+    if input_channels > 1 and input_freq > 44100:
+        bitrate = "96k"
+    elif input_freq > 44100:
+        bitrate = "80k"
+    elif input_channels > 1 and input_freq > 44000:
+        bitrate = "64k"
+    elif input_freq > 44000:
+        bitrate = "48k"
+    elif input_channels > 1 and input_freq > 22000:
+        bitrate = "32k"
+    elif input_freq > 22000:
+        bitrate = "32k"
+    elif input_channels > 1:
+        bitrate = "32k"
+    else:
+        bitrate = "32k"
+
+    bitrateMessage = "As the input file is " + str(input_freq) + "Hz"
+    bitrateMessage += " and is " + str(input_channels) + " channel"
+    bitrateMessage += ", use a bitrate of " + bitrate
+    print(bitrateMessage)
+
+def set_metadata_on_m4b_audiobook_file(self, m4b_file_path, author_name, book_name, book_folder):
+    # set metadata
+    mp4Info = MP4(m4b_file_path)
+
+    # set minimal tags
+    mp4Info['\xa9ART'] = author_name
+    mp4Info['\xa9alb'] = book_name
+
+    # set cover image
+    coverType = "none"
+    coverFile = ""
+    if os.path.isfile(book_folder + "/cover.jpg"):
+        coverFile = book_folder + "/cover.jpg"
+        coverType = 'jpg'
+    if os.path.isfile(book_folder + "/cover.png"):
+        coverFile = book_folder + "/cover.png"
+        coverType = 'png'
+
+    if coverType != "none":
+        with open(coverFile, "rb") as f:
+            if coverType == "jpg":
+                mp4Info['covr'] = [
+                    MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_JPEG)
+                ]
+            else:
+                mp4Info['covr'] = [
+                    MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_PNG)
+                ]
+
+    mp4Info.save()

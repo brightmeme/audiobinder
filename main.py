d5d00a011c09b2deecf8d8fd2bab757387d4b6af
Tom Prouse <tom.prouse@gmail.com>
2020-07-06 22:58:23 +1000

    fix error handling, introduce error folder

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 
 import os
 from audiobook import Audiobook
+import mutagen
 
 threadsToManage = 8  # How many threads should be run simultaneously
 
@@ -19,6 +20,7 @@ input_folder = "/home/tom/dev/data/audiobooks/input/"
 working_folder = "/home/tom/dev/data/audiobooks/working/"
 output_folder = "/home/tom/dev/data/audiobooks/done/"
 archive_folder = "/home/tom/dev/data/audiobooks/archive/"
+error_folder = "/home/tom/dev/data/audiobooks/error/"
 
 # find audiobooks to encode
 for entry in os.listdir(input_folder):
@@ -37,88 +39,93 @@ for entry in os.listdir(input_folder):
     book_name = bookData[2].strip()
     print("Author=", author_name, " Title=", book_name)
 
-    audiobook = Audiobook(author_name, book_name, source_folder, working_folder, output_folder, archive_folder, threadsToManage)
+    audiobook = Audiobook(author_name, book_name, source_folder, working_folder, output_folder, archive_folder, error_folder, threadsToManage)
 
-    # create working folder
-    audiobook.recode_to_mp3()
+    try:
+        audiobook.create_working_folder()
 
-    source_type = audiobook.determine_source_type()
-    print(source_type)
+        source_type = audiobook.determine_source_type()
+        print(source_type)
 
-    if source_type == audiobook.source_type_mp3_single_folder:
-        # determine bitrate - first mp3 file in source folder
-        bitrate = audiobook.determine_bitrate_from_mp3_file(audiobook.source_folder)
+        if source_type == audiobook.source_type_mp3_single_folder:
+            # determine bitrate - first mp3 file in source folder
+            bitrate = audiobook.determine_bitrate_from_mp3_file(audiobook.source_folder)
 
-        # encode all mp3 files in source folder to aac
-        audiobook.encode_mp3_files_in_source_folder(bitrate)
+            # encode all mp3 files in source folder to aac
+            audiobook.encode_mp3_files_in_source_folder(bitrate)
 
-        # pull chapter lengths from source mp3s
-        # audiobook.mp3_extract_chapter_metadata()
+            # pull chapter lengths from source mp3s
+            # audiobook.mp3_extract_chapter_metadata()
 
-        # combine all aac files in working folder to m4b
-        audiobook.merge_aac_files_in_working_folder_into_m4b()
+            # combine all aac files in working folder to m4b
+            audiobook.merge_aac_files_in_working_folder_into_m4b()
 
-        audiobook.load_chapters()
+            audiobook.load_chapters()
 
-    elif source_type == audiobook.source_type_mp3_multi_folder:
-        # copy all mp3 files to new single folder structure in working folder
-        audiobook.deepcopy_mp3_files_to_working_folder()
+        elif source_type == audiobook.source_type_mp3_multi_folder:
+            # copy all mp3 files to new single folder structure in working folder
+            audiobook.deepcopy_mp3_files_to_working_folder()
 
-        # determine bitrate - first mp3 file in working folder
-        bitrate = audiobook.determine_bitrate_from_mp3_file(audiobook.working_folder)
+            # determine bitrate - first mp3 file in working folder
+            bitrate = audiobook.determine_bitrate_from_mp3_file(audiobook.working_folder)
 
-        # encode all mp3 files in working folder to aac
-        audiobook.encode_mp3_files_in_working_folder(bitrate)
+            # encode all mp3 files in working folder to aac
+            audiobook.encode_mp3_files_in_working_folder(bitrate)
 
-        # combine all aac files in working folder to m4b
-        audiobook.merge_aac_files_in_working_folder_into_m4b()
+            # combine all aac files in working folder to m4b
+            audiobook.merge_aac_files_in_working_folder_into_m4b()
 
-        audiobook.load_chapters()
+            audiobook.load_chapters()
 
-    elif source_type == audiobook.source_type_aac_single_folder:
-        # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
+        elif source_type == audiobook.source_type_aac_single_folder:
+            # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
 
-        # copy aac files into well named aac files in working folder
-        audiobook.copy_files_to_working_folder('aac')
+            # copy aac files into well named aac files in working folder
+            audiobook.copy_files_to_working_folder('aac')
 
-        # combine all aac files in working folder to m4b
-        audiobook.merge_aac_files_in_working_folder_into_m4b()
+            # combine all aac files in working folder to m4b
+            audiobook.merge_aac_files_in_working_folder_into_m4b()
 
-    elif source_type == audiobook.source_type_m4a_single_folder:
-        # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
+        elif source_type == audiobook.source_type_m4a_single_folder:
+            # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
 
-        # copy m4a files into working folder
-        audiobook.copy_files_to_working_folder('m4a')
+            # copy m4a files into working folder
+            audiobook.copy_files_to_working_folder('m4a')
 
-        # extract aac audio from m4a
-        audiobook.extract_aac_from_files_in_working_folder('m4a')
+            # extract aac audio from m4a
+            audiobook.extract_aac_from_files_in_working_folder('m4a')
 
-        # combine all aac files in working folder to m4b
-        audiobook.merge_aac_files_in_working_folder_into_m4b()
+            # combine all aac files in working folder to m4b
+            audiobook.merge_aac_files_in_working_folder_into_m4b()
 
-    elif source_type == audiobook.source_type_m4b_single_folder:
-        # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
+        elif source_type == audiobook.source_type_m4b_single_folder:
+            # WARNING - ONLY WORKS IF ALL SOURCE FILES HAVE SAME BITRATE
 
-        # copy m4a files into working folder
-        audiobook.copy_files_to_working_folder('m4b')
+            # copy m4a files into working folder
+            audiobook.copy_files_to_working_folder('m4b')
 
-        # extract aac audio from m4a
-        audiobook.extract_aac_from_files_in_working_folder('m4b')
+            # extract aac audio from m4a
+            audiobook.extract_aac_from_files_in_working_folder('m4b')
 
-        # combine all aac files in working folder to m4b
-        audiobook.merge_aac_files_in_working_folder_into_m4b()
+            # combine all aac files in working folder to m4b
+            audiobook.merge_aac_files_in_working_folder_into_m4b()
 
 
-    # set metadata
-    audiobook.set_metadata_on_m4b_audiobook_file()
+        # set metadata
+        audiobook.set_metadata_on_m4b_audiobook_file()
 
-    # move m4b to output folder
-    audiobook.move_completed_encode_to_output_folder()
+        # move m4b to output folder
+        audiobook.move_completed_encode_to_output_folder()
 
-    # archive source folder
-    audiobook.archive_source_files()
+        # archive source folder
+        audiobook.archive_source_files()
 
-    # clear working folder
-    audiobook.clear_working_folder()
+    # except mutagen.mp3.HeaderNotFoundError:
+    #     audiobook.error_handle_mutagenmp3headernotfound()
+
+    finally:
+
+        # clear working folder
+        audiobook.clear_working_folder()
 
 
